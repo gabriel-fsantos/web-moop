f90b5c49ff9a3c59244ef42740212c6866d9bd9a
Gabriel Ferreira dos Santos <biel0fds@gmail.com>
2020-02-27 11:56:35 -0300

    continuação

--- moop_script.js
+++ moop_script.js
@@ -79,14 +79,14 @@ function sendRequest() {
     }
 }
 
-valida(function() {
+(function() {
     'use strict';
     window.addEventListener('load', function() {
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
       var forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
       var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+        form.addEventListener("click", function(event) {
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -95,4 +95,4 @@ valida(function() {
         }, false);
       });
     }, false);
-})();
+  })();
